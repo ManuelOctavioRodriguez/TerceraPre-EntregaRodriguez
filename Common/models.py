7b279ec29570a1b8c94c82eeb942f93576db6ceb
Manuel Rodriguez <manueloctaviorodriguez1995@gmail.com>
2023-05-06 14:01:11 -0300

    Agregar cambios

--- Common/models.py
+++ Common/models.py
@@ -11,3 +11,10 @@ class Textos(models.Model):
     titulo = models.CharField(max_length=40)
     texto = models.CharField(max_length=480)
     image = models.ImageField(upload_to='images/', blank=True)
+    autor = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+
+
+class Comentarios(models.Model):
+    comentario = models.CharField(max_length=240)
+    autor = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    texto = models.ForeignKey(Textos, on_delete=models.CASCADE)
