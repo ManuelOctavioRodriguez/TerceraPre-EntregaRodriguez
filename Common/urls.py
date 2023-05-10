b494e9d3430f12001214aad8732bb63ffceafd21
Manuel Rodriguez <manueloctaviorodriguez1995@gmail.com>
2023-05-10 17:31:48 -0300

    Cambios

--- Common/urls.py
+++ Common/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from Common.views import Index, listaTextos, listaUsuarios, crearUsuario
+from Common.views import Index, listaTextos, listaUsuarios, crearUsuario, crearTexto
 
 urlpatterns = [
     path('', Index, name="Index"),
-    path('usuarios/', listaUsuarios),
+    path('Usuarios/', listaUsuarios),
     path('textos/', listaTextos),
     path('crearUsuario/', crearUsuario),
+    path('crearTexto/', crearTexto),
 ]
